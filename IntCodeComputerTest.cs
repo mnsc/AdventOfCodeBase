e4ebdeec32f9770a3373f9f4a680c9663fb2afb2
Mattias Blom <mattias.blom@gmail.com>
2019-12-14 18:46:36 +0100

    Adding tests to the same project instead.

---
+++ IntCodeComputerTest.cs
@@ -0,0 +1,37 @@
+﻿using AdventOfCode.Solutions.Year2019;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace AdventOfCode
+{
+    public class IntCodeComputerTest
+    {
+        [Theory]
+        [InlineData(
+            new int[] { 1, 9, 10, 3, 2, 3, 11, 0, 99, 30, 40, 50 },
+            new int[] { 3500, 9, 10, 70, 2, 3, 11, 0, 99, 30, 40, 50 })]
+        [InlineData(
+            new int[] { 1, 0, 0, 0, 99 },
+            new int[] { 2, 0, 0, 0, 99 })]
+        [InlineData(
+            new int[] { 2, 3, 0, 3, 99 },
+            new int[] { 2, 3, 0, 6, 99 })]
+        [InlineData(
+            new int[] { 2, 4, 4, 5, 99, 0 },
+            new int[] { 2, 4, 4, 5, 99, 9801 })]
+        [InlineData(
+            new int[] { 1, 1, 1, 4, 99, 5, 6, 0, 99 },
+            new int[] { 30, 1, 1, 4, 2, 5, 6, 0, 99 })]
+        public void BasicFromDay2(int[] memory, int[] desiredEndState)
+        {
+            IntCodeComputer.RunOnMemory(memory);
+            Assert.Equal(memory, desiredEndState);
+        }
+
+       
+
+    }
+
+}
