ae60a63f511592eeaa113c156e6dbe85afc64273
Mattias Blom <mattias.blom@gmail.com>
2019-12-15 10:28:20 +0100

    Test for the large example.

--- IntCodeComputerTest.cs
+++ IntCodeComputerTest.cs
@@ -115,8 +115,6 @@ namespace AdventOfCode
         [InlineData(
             new int[] { 3, 3, 1108, -1, 8, 3, 4, 3, 99 },
             88, 0)]
-        
-
         public void Check_if_input_is_8(int[] memory, int input, int expectedOutput)
         {
             var outputs = IntCodeComputer.RunOnMemory(memory, input);
@@ -137,8 +135,6 @@ namespace AdventOfCode
         [InlineData(
             new int[] { 3, 3, 1107, -1, 8, 3, 4, 3, 99 },
             8, 0)]
-        
-
         public void Check_if_input_is_less_than_8(int[] memory, int input, int expectedOutput)
         {
             var outputs = IntCodeComputer.RunOnMemory(memory, input);
@@ -146,6 +142,21 @@ namespace AdventOfCode
             Assert.Equal(expectedOutput, outputs[0]);
         }
 
+
+        [Theory]
+        [InlineData(4, 999)]
+        [InlineData(8, 1000)]
+        [InlineData(19, 1001)]
+        public void Larger_Example_from_day_5_part_2(int input, int expectedOutput)
+        {
+            var memory = new int[] { 
+                3,21,1008,21,8,20,1005,20,22,107,8,21,20,1006,20,31,
+                1106,0,36,98,0,0,1002,21,125,20,4,20,1105,1,46,104,
+                999,1105,1,46,1101,1000,1,20,4,20,1105,1,46,98,99 };
+
+            var outputs = IntCodeComputer.RunOnMemory(memory, input);
+            Assert.Equal(expectedOutput, outputs[0]);
+        }
     }
 
 }
